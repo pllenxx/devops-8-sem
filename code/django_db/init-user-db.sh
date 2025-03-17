c6573f8ab3a29fc04a266cb58c3919dad34db010
Polina <khartanovichp@mail.ru>
2025-03-18 02:37:02 +0300

    fix: postgres and api

--- code/django_db/init-user-db.sh
+++ code/django_db/init-user-db.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+if [ ! -s "$PGDATA/PG_VERSION" ]; then
+    echo "Init PostgreSQL database"
+	initdb --username="$POSTGRES_USER" --pwfile=<(echo "$POSTGRES_PASSWORD")
+
+    echo "host all all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+    echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
+    pg_ctl start
+
+    if [ "$POSTGRES_DB" ]; then
+        createdb --username="$POSTGRES_USER" "$POSTGRES_DB"
+    fi
+
+    if [ "$POSTGRES_USER" ] && [ "$POSTGRES_PASSWORD" ]; then
+        psql --username="$POSTGRES_USER" --dbname="$POSTGRES_DB" --command "ALTER USER \"$POSTGRES_USER\" WITH PASSWORD '$POSTGRES_PASSWORD';"
+    fi
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE test_app;
-	GRANT ALL PRIVILEGES ON DATABASE test_app TO postgres;
-EOSQL
+	GRANT ALL PRIVILEGES ON DATABASE test_app TO $POSTGRES_USER;
+	EOSQL
+
+    pg_ctl stop
+fi
+
+exec postgres
--- code/django_rest_api/run-migrate-and-server.sh
+++ code/django_rest_api/run-migrate-and-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sleep 5
+source $APP_HOME/venv/bin/activate
 python test_app/manage.py migrate
 python test_app/manage.py loaddata profiles
 python test_app/manage.py runserver 0:8000
